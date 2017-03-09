a1c8cf8863d40bce94af0191bc3776745c22045b
dn1412maa <alexander.mustafin@gmail.com>
2017-03-09 11:55:55 +0200

    Create test-rdseventshandler.py

---
+++ test-rdseventshandler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import boto3
+import boto
+import datetime
+import unittest
+import json
+import lambda_function
+from mock import Mock, patch
+from moto import mock_sns
+
+
+
+
+print("YES")
